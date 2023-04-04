b154d80722cae652db028dd2db2983450fb23072
DuongKien2001 <trungkienty2001@gmail.com>
2023-04-04 21:54:08 +0700

    k

--- train.py
+++ train.py
@@ -51,10 +51,10 @@ def train(gpu, args, cfg):
         logger.info(type(model_A))
         logger.info(trainer)
 
-    for e in range(cfg.SOLVER.MAX_EPOCHS-9):
+    for e in range(cfg.SOLVER.MAX_EPOCHS-19):
 
-        source_train_sampler.set_epoch(e+9)
-        target_train_sampler.set_epoch(e+9)
+        source_train_sampler.set_epoch(e+19)
+        target_train_sampler.set_epoch(e+19)
 
         for batch in (tqdm(trainer.tt_dl) if rank==0 else trainer.tt_dl):
             trainer.step(batch)
--- trainer.py
+++ trainer.py
@@ -79,7 +79,7 @@ class BaseTrainer(object):
         self.res_recoder_A = result_recorder('mean_model_A')
         self.res_recoder_B = result_recorder('mean_model_B')
 
-        self.train_epoch = 10
+        self.train_epoch = 20
         self.optim_A = optimizer_A
         self.optim_B = optimizer_B
         self.scaler = scaler
@@ -100,7 +100,7 @@ class BaseTrainer(object):
             summary_dir = os.path.join(cfg.OUTPUT_DIR, 'summaries/')
             os.makedirs(summary_dir, exist_ok=True)
             self.summary_writer = SummaryWriter(log_dir=summary_dir)
-        self.current_iteration = 744*9
+        self.current_iteration = 744*19
 
         self.mean_model_A = torch.optim.swa_utils.AveragedModel(self.model_A, device=gpu)
         self.mean_model_B = torch.optim.swa_utils.AveragedModel(self.model_B, device=gpu)
